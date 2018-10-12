7eb3255b37411f43c72e61bd6248892ea2004eb9
Aren Chen <yarencheng@gmail.com>
2018-10-12 10:10:58 -0700

    change default value

--- main.cpp
+++ main.cpp
@@ -28,9 +28,9 @@ int main(int argc, char** argv) {
     options.add_options()
         ("help",                                                            "Show help message")
         ("profiling",                                                       "Profile the time consumption of this filter")
-        ("data-size-min",   value<int>(&min)->default_value(10000),             "Min. number of dirty words used in the profiling")
-        ("data-size-max",   value<int>(&max)->default_value(200000),            "Max. number of dirty words used in the profiling")
-        ("data-size-step",  value<int>(&step)->default_value(10000),             "Number of dirty words added between each profiling")
+        ("data-size-min",   value<int>(&min)->default_value(100000),             "Min. number of dirty words used in the profiling")
+        ("data-size-max",   value<int>(&max)->default_value(2000000),            "Max. number of dirty words used in the profiling")
+        ("data-size-step",  value<int>(&step)->default_value(100000),             "Number of dirty words added between each profiling")
         ("sentence-length", value<int>(&len)->default_value(1000),             "Length of the sentence to be filtered in the profiling")
         ("report-file",     value<string>(&report)->default_value("report.csv"),   "Length of the sentence to be filtered in the profiling");
 
